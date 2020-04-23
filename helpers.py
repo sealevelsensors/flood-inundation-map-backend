667ea2ea4e2374bb75aa7a38734a1f54e07deba5
Akhil <akhil@IPaTs-MacBook-Pro.local>
2020-04-23 05:50:12 -0700

    implement helpfer functions for inundation calculations

--- helpers.py
+++ helpers.py
@@ -86,3 +86,129 @@ def read_blob(bucket_name, source_blob_name):
 
   ret = json.loads(blob.download_as_string())
   return ret
+
+
+def DoOA(lon_d, lat_d, data, lon_gr, lat_gr, xcorr, ycorr, errcomp):
+    I = len(data[:])
+    datasize = I
+
+    #     compute the spatial mean of the data by fitting a plane mean = a + b*lon + c*lat where
+    #     [a,b,c]=m is the vector of the coefficients
+    d, m = RemoveGlobalMean(lon_d, lat_d, data)
+
+    #     Compute Covariance Matrices using Gaussian ofr x and y correlation
+    #     Set correlation scale for Covariance Matrix
+    a = xcorr
+    b = ycorr
+
+    #     Scales are set same as Chereskin et al. JGR Vol. 101 pag. 22,619-22,629, OCT 15,1996
+
+    #     Data - Grid Covariance = GD
+    GD = GaussianCovariance(lon_d, lon_gr, lat_d, lat_gr, a, b)
+
+    #     Data - Data Covariance = DD
+    DD = GaussianCovariance(lon_d, lon_d, lat_d, lat_d, a, b)
+    DD = DD + np.eye(datasize) * 0.01
+    A = np.linalg.lstsq(DD.T, GD)[0].T
+    d_gr = A @ d
+
+    #     Setup bidimensional arrays for plotting
+    field = d_gr
+
+    [I, J] = lon_gr.shape
+    field = field.reshape(I, J, order='F')
+
+    #     Add the mean back
+    mean_gr = AddGlobalMean(lon_gr, lat_gr, m)
+    field = field + mean_gr
+
+    errmap = 0
+    if errcomp == 1:
+        #     Compute error map
+        #     Grid - Grid Covariance = GG
+        GG = GaussianCovariance(lon_gr, lon_gr, lat_gr, lat_gr, a, b)
+        E = GG - A @ GD
+        dE = np.diag(E)
+        errmap = dE.reshape(I, J, order='F')
+
+    return field, errmap
+
+
+# Build gaussian covariance
+# exp(- ((x-x1)/a)^2 - ((y-y1)/b)^2)
+def GaussianCovariance(x, x1, y, y1, a, b):
+    i = x.size
+    j = x1.size
+    X = M2d(x, j)
+    Y = M2d(y, j)
+    X1 = M2d(x1, i).T
+    Y1 = M2d(y1, i).T
+
+    sio.savemat('X_test.mat', {'arr': X})
+    sio.savemat('X1_test.mat', {'arr': X1})
+    sio.savemat('Y_test.mat', {'arr': Y})
+    sio.savemat('Y1_test.mat', {'arr': Y1})
+
+    GaCOV = (((X - X1) * (X - X1)) * (1 / (a * a))) + (((Y - Y1) * (Y - Y1)) * (1 / (b * b)))
+    GaCOV = np.exp(-GaCOV)
+
+    return GaCOV
+
+
+# helper function for Gaussian Covariance
+def M2d(x, J):
+    I = x.size
+    X = np.zeros((I, 1))
+    X[:, 0] = x.flatten(1)
+    X = np.tile(X, (1, J))
+
+    #     I=x.size
+    #     X=np.zeros((I,1))
+    #     X[:]=x[:]
+    #     X=np.tile(X,(1,J))
+    return X
+
+
+# normalize the data by removing the mean
+def RemoveGlobalMean(lon_d, lat_d, data):
+    find = np.where(~np.isnan(data))[0]
+    data = data[find]
+    lon_d = lon_d[0][find]
+    lat_d = lat_d[0][find]
+
+    #     set up G
+    I = data.size
+    datasize = I
+    G = np.zeros((I, 3))
+    G[:, 0] = lon_d[:]
+    G[:, 1] = lat_d[:]
+    G[:, 2] = 1
+
+    #     set up d
+    d = data[:]
+
+    CME = G.T @ G + np.diag([1, 1, 1]) * 0.0001
+    CME.shape
+
+    A = np.linalg.inv(CME)
+    b = G.T @ d
+    m = A @ b
+    d = G @ m
+
+    d = data - d
+
+    return d, m
+
+
+def AddGlobalMean(lon_gr, lat_gr, m):
+    Ihat = len(lon_gr.flatten(1))
+    G = 0
+    G = np.zeros((Ihat, 3))
+    G[:, 0] = lon_gr.flatten(1)
+    G[:, 1] = lat_gr.flatten(1)
+    G[:, 2] = 1
+
+    mean_gr = G @ m
+    [I, J] = lon_gr.shape
+    mean_gr = mean_gr.reshape(I, J, order='F')
+    return mean_gr
